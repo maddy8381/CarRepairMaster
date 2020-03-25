42a78cd9a283a1ed3ba04d6a59dc1b4af080a97c
unknown <sgmadankar@gmail.com>
2020-03-25 17:56:04 +0530

    added Header & Navbar components
    Added basic routing for all components, added WildCard Route
    
    Note: Previous commits add only project setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { LoginComponent } from './login/login.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path : '', redirectTo : '/home', pathMatch : 'full'}, /*WildCart Routing */
+  { path : 'home', component : HomeComponent},
+  { path : 'about-us', component : AboutUsComponent},
+  { path : 'services', component : ServicesComponent},
+  { path : 'contact-us', component : ContactUsComponent},
+  { path : 'login', component : LoginComponent},
+  { path : '**', component : PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routingComponents = []
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    AboutUsComponent,
+    ServicesComponent,
+    ContactUsComponent,
+    LoginComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
